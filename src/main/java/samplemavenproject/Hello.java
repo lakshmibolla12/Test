fa20c3d67b92518045a9dc6dd62960033ca818f0
LakshmiBolla <nagalakshmi.bolla@jobsnprofiles.com>
2024-02-28 10:07:55 -0800

    msg

---
+++ src/main/java/samplemavenproject/Hello.java
@@ -0,0 +1,11 @@
+package samplemavenproject;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing git maven tomcat setup");
+
+	}
+
+}
